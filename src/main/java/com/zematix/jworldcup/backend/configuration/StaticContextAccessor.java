43d46b2b46e07109f8a42d421c50e2f09bed7248
Zsolt Branyiczky <brazso@zematix.hu>
2022-11-11 16:46:11 +0100

    fix: application did not start in production mode due to StaticContextAccessor bad timing

--- src/main/java/com/zematix/jworldcup/backend/configuration/StaticContextAccessor.java
+++ src/main/java/com/zematix/jworldcup/backend/configuration/StaticContextAccessor.java
@@ -1,26 +1,72 @@
 package com.zematix.jworldcup.backend.configuration;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
+/**
+ * Static accessor to ApplicationContext object.
+ * @see <a href="https://foojay.io/today/statically-spilling-your-spring-beans/">Statically Spilling Your (Spring) Beans</a>
+ */
 @Component
 public class StaticContextAccessor {
 
-    private static StaticContextAccessor instance;
+    private static final Map<Class, DynamicInvocationhandler> classHandlers = new HashMap<>();
+    private static ApplicationContext context;
 
     @Autowired
-    private ApplicationContext applicationContext;
-
-    @PostConstruct
-    public void registerInstance() {
-        instance = this;
+    public StaticContextAccessor(ApplicationContext applicationContext) {
+        context = applicationContext;
     }
 
     public static <T> T getBean(Class<T> clazz) {
-        return instance.applicationContext.getBean(clazz);
+        if (context == null) {
+            return getProxy(clazz);
+        }
+        return context.getBean(clazz);
     }
 
+    private static <T> T getProxy(Class<T> clazz) {
+        DynamicInvocationhandler<T> invocationhandler = new DynamicInvocationhandler<>();
+        classHandlers.put(clazz, invocationhandler);
+        return (T) Proxy.newProxyInstance(
+                clazz.getClassLoader(),
+                new Class[]{clazz},
+                invocationhandler
+        );
+    }
+
+    //Use the context to get the actual beans and feed them to the invocationhandlers
+    @PostConstruct
+    private void init() {
+        classHandlers.forEach((clazz, invocationHandler) -> {
+            Object bean = context.getBean(clazz);
+            invocationHandler.setActualBean(bean);
+        });
+    }
+
+    static class DynamicInvocationhandler<T> implements InvocationHandler {
+
+        private T actualBean;
+
+        public void setActualBean(T actualBean) {
+            this.actualBean = actualBean;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (actualBean == null) {
+                throw new RuntimeException("Not initialized yet! :(");
+            }
+            return method.invoke(actualBean, args);
+        }
+    }
 }
