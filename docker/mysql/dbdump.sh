2cd1dc3c6949629660cbf8792620cac1b75c2564
Zsolt Branyiczky <brazso@zematix.hu>
2022-11-21 16:40:11 +0100

    mysql docker changes

--- docker/mysql/dbdump.sh
+++ docker/mysql/dbdump.sh
@@ -1,12 +1,2 @@
-#!/usr/bin/bash
-
-# add /root/.my.cnf file for mysqldump usage without password
-# [mysqldump]
-cat > .my.cnf <<EOL
-[mysqldump]
-user=worldcup
-password=secret
-EOL
-#
-
-mysqldump -u worldcup --no-tablespaces --single-transaction --quick --lock-tables=false worldcup | gzip > "/root/worldcup.zematix.hu/mysql/backup/worldcup_`date +"%Y%m%d%H%M%S"`.sql.gz"
+#!/bin/bash
+mysqldump -u worldcup --no-tablespaces --single-transaction --quick --lock-tables=false worldcup | gzip > "/root/jworldcup/mysql/backup/worldcup_`date +"%Y%m%d%H%M%S"`.sql.gz"
