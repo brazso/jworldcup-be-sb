21c448efd9578c5ea3971ab4813602d363c67d56
Zsolt Branyiczky <brazso@zematix.hu>
2022-12-01 11:44:01 +0100

    new docker service named backup to support scheduled database backups in production mode

---
+++ docker/backup/backup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Get hostname: try read from file, else get from env
+[ -z "${MYSQL_HOST_FILE}" ] || { MYSQL_HOST=$(head -1 "${MYSQL_HOST_FILE}"); }
+[ -z "${MYSQL_HOST}" ] && { echo "=> MYSQL_HOST cannot be empty" && exit 1; }
+# Get username: try read from file, else get from env
+[ -z "${MYSQL_USER_FILE}" ] || { MYSQL_USER=$(head -1 "${MYSQL_USER_FILE}"); }
+[ -z "${MYSQL_USER}" ] && { echo "=> MYSQL_USER cannot be empty" && exit 1; }
+# Get password: try read from file, else get from env, else get from MYSQL_PASSWORD env
+[ -z "${MYSQL_PASS_FILE}" ] || { MYSQL_PASS=$(head -1 "${MYSQL_PASS_FILE}"); }
+[ -z "${MYSQL_PASS:=$MYSQL_PASSWORD}" ] && { echo "=> MYSQL_PASS cannot be empty" && exit 1; }
+# Get database name(s): try read from file, else get from env
+# Note: when from file, there can be one database name per line in that file
+[ -z "${MYSQL_DATABASE_FILE}" ] || { MYSQL_DATABASE=$(cat "${MYSQL_DATABASE_FILE}"); }
+# Get level from env, else use 6
+[ -z "${GZIP_LEVEL}" ] && { GZIP_LEVEL=6; }
+[ -z "${DATE_FORMAT}" ] && { DATE_FORMAT="date +%Y%m%d%H%M"; }
+[ -z "${FILENAME_FORMAT}" ] && { FILENAME_FORMAT="/backup/\$DATE.\$db.sql"; }
+[ -z "${LATEST_FORMAT}" ] && { LATEST_FORMAT="/backup/latest.\$db.sql"; }
+
+DATE=$(${DATE_FORMAT})
+echo "=> Backup started at $(date "+%Y-%m-%d %H:%M:%S")"
+DATABASES=${MYSQL_DATABASE:-${MYSQL_DB:-$(mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" $MYSQL_SSL_OPTS -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)}}
+for db in ${DATABASES}
+do
+  if  [[ "$db" != "information_schema" ]] \
+      && [[ "$db" != "performance_schema" ]] \
+      && [[ "$db" != "mysql" ]] \
+      && [[ "$db" != "sys" ]] \
+      && [[ "$db" != _* ]]
+  then
+    echo "==> Dumping database: $db"
+    FILENAME=$(eval echo $FILENAME_FORMAT)
+    LATEST=$(eval echo $LATEST_FORMAT)
+    if mysqldump --single-transaction $MYSQLDUMP_OPTS -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" $MYSQL_SSL_OPTS "$db" > "$FILENAME"
+    then
+      EXT=
+      if [ -z "${USE_PLAIN_SQL}" ]
+      then
+        echo "==> Compressing $db with LEVEL $GZIP_LEVEL"
+        gzip "-$GZIP_LEVEL" -f "$FILENAME"
+        EXT=.gz
+        FILENAME=$FILENAME$EXT
+        LATEST=$LATEST$EXT
+      fi
+      BASENAME=$(basename "$FILENAME")
+      echo "==> Creating symlink to latest backup: $BASENAME"
+      rm "$LATEST" 2> /dev/null
+      cd /backup || exit && ln -s "$BASENAME" "$(basename "$LATEST")"
+      if [ -n "$MAX_BACKUPS" ]
+      then
+        while [ "$(find /backup -maxdepth 1 -name "*.$db.sql$EXT" -type f | wc -l)" -gt "$MAX_BACKUPS" ]
+        do
+          TARGET=$(find /backup -maxdepth 1 -name "*.$db.sql$EXT" -type f | sort | head -n 1)
+          echo "==> Max number of ($MAX_BACKUPS) backups reached. Deleting ${TARGET} ..."
+          rm -rf "${TARGET}"
+          echo "==> Backup ${TARGET} deleted"
+        done
+      fi
+    else
+      rm -rf "$FILENAME"
+    fi
+  fi
+done
+echo "=> Backup process finished at $(date "+%Y-%m-%d %H:%M:%S")"
