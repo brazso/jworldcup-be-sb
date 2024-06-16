df8f25d47cf4f918d1502ed0536083af82b979ea
Zsolt Branyiczky <brazso@zematix.hu>
2024-06-16 13:28:20 +0200

    beutifying

--- src/main/java/com/zematix/jworldcup/backend/tool/OpenLigaDBEvent.java
+++ src/main/java/com/zematix/jworldcup/backend/tool/OpenLigaDBEvent.java
@@ -2,7 +2,6 @@ package com.zematix.jworldcup.backend.tool;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
