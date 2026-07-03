92d7a5cd8b2d1c53bcfe34f7cd6480944e552f15
Zsolt Branyiczky <brazso@zematix.hu>
2026-07-03 18:33:04 +0200

    fix: hide remote mysql 3306 port

--- docker/phpmyadmin/config.user.inc.php
+++ docker/phpmyadmin/config.user.inc.php
@@ -4,19 +4,14 @@ $cfg['Servers'] = [
     1 => [
         'auth_type' => 'cookie',
         'host' => 'localhost',
-        //'port' => 3306,
+        'port' => 3306,
         'socket' => '/var/run/mysqld/mysqld.sock',
-        'verbose' => 'Local developer',
+        'verbose' => 'Local developer'
     ],
     2 => [
-        'auth_type' => 'cookie',
-        'host' => 'worldcup.zematix.hu',
-        //'port' => 3306,
-        //'ssl' => true,
-        //'ssl_verify' => false,
-        //'ssl_key' => '../client-key.pem',
-		//'ssl_cert' => '../client-cert.pem',
-		//'ssl_ca' => '../server-ca.pem',
-        'verbose' => 'Remote production',
+        'host' => 'host.docker.internal',
+        'port' => 3307,
+        'connect_type' => 'tcp',
+        'verbose' => 'Remote production via SSH tunnel'
     ],
 ];
